3aa30e19ca9f687bcce6c55a13902666fcd77376
MichelleConceição <liliandlentretenimento@gmail.com>
2022-06-17 14:37:35 -0300

    desafio funções

---
+++ script.js
@@ -0,0 +1,27 @@
+function mensagem(exibi){
+    console.log("Hoje esta muito frio");
+}
+
+mensagem()
+
+function nome(nome){
+    console.log(nome);
+}
+
+nome("Michelle")
+
+function nome2(nome){
+    return nome;
+}
+
+function dados(nome, idade, estiloMusical){
+    console.log(nome, idade, estiloMusical);
+}
+
+dados("Michelle", "32", "Rock");
+
+function gosto(filme, musica){
+    console.log(filme, musica);
+}
+
+gosto("O Menino do Pijama Listrado", "trevo");
